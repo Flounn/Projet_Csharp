eecd86d6409c215e5c485555451a2b628ba42615
lupflamind <eugen.tiganu@gmail.com>
2015-04-07 15:01:13 +0200

    build

--- PRJMIDODesktop/DesktopIHM/GuiObjects/FenDetailClient.cs
+++ PRJMIDODesktop/DesktopIHM/GuiObjects/FenDetailClient.cs
@@ -6,35 +6,37 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DataService.BSDataObjects;
 
 namespace DesktopIHM.GuiObjects
 {
     public partial class FenDetailClient : Form
     {
-        private Client monClient;        
+        private Client monClient;
 
         public Client MonClient
         {
             get { return monClient; }
             set { monClient = value; }
         }
-        
-        public FenDetailClient()
+
+        public FenDetailClient(Client cli)
         {
             InitializeComponent();
-            maPersonne = p;
+            monClient = cli;
             initData();
         }
-    
+
 
         private void initData()
         {
-            if(maPersonne!=null)
+            if (monClient != null)
             {
-                this.txtNom.text=maPersonne.nom;
-                this.txtPrenom.text=maPersonne.Prenom;
-                this.txtAxe.text=maPersonne.age.toString();
-                this.txtEmail.text=maPersonne.email;
+                this.txtNom.Text= monClient.Nom;
+                this.txtPrenom.Text = monClient.Prenom;
+                this.txtAge.Text = monClient.DateNaissance.ToString();
+                this.txtEmail.Text = monClient.Email;
             }
         }
     }
+}
